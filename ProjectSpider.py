3ff129b94d45bc071e75ebe54b834de9bc58fd3e
nafeng <naf19891206>
2018-04-12 23:43:37 +0800

    完成项目基本信息抓取和导入数据库功能

---
+++ ProjectSpider.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from HtmlSpider import HtmlSpider
+
+attrMap = {
+    "工程名称:": "projectName",
+    "项目类型:": "projectType",
+    "合同价(万元):": "orderAmount",
+    "结算价(万元):": "finalAmount",
+    "技术等级:": "techLevel",
+    "合同段名称:": "contractSegmentName",
+    "开工日期:": "startDate",
+    "交工日期:": "endDate",
+    "竣工日期:": "completeDate",
+    "建设状态:": "constructionState",
+    "合同段开始桩号:": "startSegment",
+    "合同段结束桩号:": "endSegment",
+    "质量评定情况:": "quality",
+    "所在省份:": "province",
+    "项目代码:": "code",
+    "主要工程量:": "projectQuantities",
+}
+
+
+class ProjectSpider(HtmlSpider):
+    def __init__(self, company_id, project_id):
+        super(ProjectSpider, self).__init__(
+            "http://glxy.mot.gov.cn/BM/CptInfoAction_outstandingShow.do?nodeType=MAINPRJ&pageType=2&corpCode=%s&keyWord=%s" % (
+            company_id, project_id))
+        company_info_table = self.html_doc.find(class_="infogrid")
+        print(company_info_table)
+        self.projectInfo = {}
+        for tr in company_info_table.find_all("tr"):
+            tds = tr.find_all("td")
+            for index in range(len(tds)):
+                if index % 2 == 0:
+                    print "\"%s\":" % tds[index].get_text().strip().encode("utf-8")
+                    try:
+                        self.projectInfo[attrMap[tds[index].get_text().strip().encode("utf-8")]] \
+                            = tds[index + 1].get_text().strip()
+                    except:
+                        continue
+        print self.projectInfo
--- ProjectWorker.py
+++ ProjectWorker.py
@@ -1,6 +1,6 @@
 import MySQLdb
 
-from CompanySpider import CompanySpider
+from ProjectSpider import ProjectSpider
 from SqlWorker import SqlWorker
 
 db = MySQLdb.connect('101.132.159.215', 'yuyao', 'yy123123', 'demo')
@@ -8,15 +8,15 @@ db.set_character_set('utf8')
 cursor = db.cursor()
 
 
-class CompanyWorker:
+class ProjectWorker:
     def __init__(self):
-        cursor.execute("select id from corp_details;")
+        cursor.execute("select corpid,proj_id,proj_type  from corp_proj;")
         records = cursor.fetchall()
         for item in records:
             print item
-            spider = CompanySpider(item[0])
-            sql = "update corp_details set %s  where id='%s'" % (SqlWorker.map_to_sql(spider.companyInfo), item[0])
+            spider = ProjectSpider(item[0], item[1])
+            sql = "update corp_details set %s  where id='%s'" % (SqlWorker.map_to_sql(spider.projectInfo), item[0])
             print sql
-            cursor.execute(sql)
+            # cursor.execute(sql)
         db.commit()
         db.close()
